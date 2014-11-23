398a4a8189e9bce55f6bc80861a0260a463e5a00
Angel <angel@angel-ThinkPad-T500.(none)>
2014-11-23 20:08:28 +0100

    Testing footer changes 3

--- .openshift/themes/radiate/footer.php
+++ .openshift/themes/radiate/footer.php
@@ -16,7 +16,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'radiate_credits' ); ?>
-			<?php _e( 'Proudly  powered byyy ', 'radiate' ); ?>
+			<?php _e( 'Proudly  powered byyyy ', 'radiate' ); ?>
 			<a href="http://wordpress.org/" rel="generator"><?php _e( 'WordPress', 'radiate' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s.', 'radiate' ), 'Radiate', '<a href="'.esc_url('http://themegrill.com/').'" rel="designer">ThemeGrill</a>' ); ?>
